1a0843342752ab511a6109ce4242c68f5d538d39
Patrick Plenefisch <simonpatp@gmail.com>
2024-05-21 22:25:59 -0400

    Update with fixes for building and subspawn CI environment

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # linux_cross_openbsd
 On Linux, cross compile program for OpenBSD 
+
+This is modified for JRuby FFI/SubSpawn build processes
+
+This is modified from upstream with:
+ * Working non-Japan URLs
+ * SSL fixes for downloading GCC dependencies
+ * Exposes ld.so so that autotools works correctly
+ * Environment fixes to more closely match dockcross
+
+It is published as `byteit101/jrubycrossopenbsd_x86_64` on docker hub.
--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -o vendor/gcc-10.2.0.tar.xz http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-10.2.0/gcc-10.2.0.tar.xz
+curl -o vendor/gcc-10.2.0.tar.xz https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz
 curl -o vendor/binutils-2.36.1.tar.xz https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.xz
-curl -o vendor/base68.tgz http://core.ring.gr.jp/archives/OpenBSD/6.8/amd64/base68.tgz
-curl -o vendor/comp68.tgz http://core.ring.gr.jp/archives/OpenBSD/6.8/amd64/comp68.tgz
+curl -o vendor/base68.tgz https://ftp.eu.openbsd.org/pub/OpenBSD/6.8/amd64/base68.tgz
+curl -o vendor/comp68.tgz https://ftp.eu.openbsd.org/pub/OpenBSD/6.8/amd64/comp68.tgz
